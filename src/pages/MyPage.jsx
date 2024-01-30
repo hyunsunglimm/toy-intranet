b54d860d0383884aa3e17cd9ac78e6b643f7f6af
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-01-30 15:37:42 +0900

    mypagemj

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -3,10 +3,20 @@ import { EmployeeContext } from "../context/EmployeeContext";
 
 export default function MyPage() {
   const { loginUser } = useContext(EmployeeContext);
+  console.log(loginUser);
 
   return (
     <section className="p-8">
-      <img src={loginUser?.image} alt="" />
+      <p>MyPage</p>
+      <div className="text-center bg-white">
+        <img className="w-72 h-80" src={loginUser?.image} alt="user" />
+        <p className="text-2xl">{loginUser?.name}</p>
+        <p>{loginUser?.age}</p>
+        <p>{loginUser?.department}</p>
+        <p>{loginUser?.email}</p>
+        <p>{loginUser?.isworking ? "부재중" : "근무중"}</p>
+      </div>
     </section>
   );
 }
+
