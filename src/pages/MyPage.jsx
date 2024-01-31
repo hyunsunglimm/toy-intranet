cbf3b61dc45f092e16fbfc569be6230d1d09f8fe
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-01-31 17:43:29 +0900

    mypage styling

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -3,13 +3,21 @@ import { EmployeeContext } from "../context/EmployeeContext";
 
 export default function MyPage() {
   const { loginUser } = useContext(EmployeeContext);
-  console.log(loginUser);
+  // console.log(loginUser);
 
   return (
-    <section className="p-8">
-      <p>MyPage</p>
-      <div className="text-center bg-white">
-        <img className="w-72 h-80" src={loginUser?.image} alt="user" />
+  
+    <section className="flex w-[100%] bg-[#728395]">
+      <div className="p-8 w-[60%] bg-[#728395] bg-blend-multiply"
+      style={{backgroundImage: `url(${loginUser?.image})`, // 이미지 파일의 경로
+      backgroundSize: "cover", // 이미지를 컨테이너에 맞게 조절
+      backgroundPosition: "center",
+      backgroundAttachment: "local",
+      backgroundRepeat: "no-repeat"}}>
+        <p>MyPage</p>
+      </div>
+      <div className="text-center bg-white right-0 w-[50%] p-10 m-14">
+        <img className="w-72 h-80 mx-auto rounded-full" src={loginUser?.image} alt="user" />
         <p className="text-2xl">{loginUser?.name}</p>
         <p>{loginUser?.age}</p>
         <p>{loginUser?.department}</p>
