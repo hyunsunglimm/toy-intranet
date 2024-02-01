c474e4d0352659838baa25fca47e4c9fb03d9f0c
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-02-01 13:33:31 +0900

    icon install

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,5 +1,6 @@
 import { useContext, useState } from "react";
 import { EmployeeContext } from "../context/EmployeeContext";
+import { MdOutlineMail } from "react-icons/md";
 
 export default function MyPage() {
   const { loginUser } = useContext(EmployeeContext);
@@ -9,25 +10,40 @@ export default function MyPage() {
     setIsWorking(!isWorking);
   };
 
+  const statusStyle = {
+    backgroundColor: isWorking ? "#FF0000" : "#00FF00", // 연두색 또는 빨간색 배경색을 선택
+    color: "#FFFFFF", // 텍스트 색상을 흰색으로 설정
+  };
+
   return (
     <section className="flex w-[100%] bg-[#728395]">
-      <div className="p-8 w-[60%] bg-[#728395] bg-blend-multiply"
+      <div
+        className="p-8 w-[60%] bg-[#728395] bg-blend-multiply"
         style={{
           backgroundImage: `url(${loginUser?.image})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundAttachment: "local",
-          backgroundRepeat: "no-repeat"
-        }}>
+          backgroundRepeat: "no-repeat",
+        }}
+      >
         <p>MyPage</p>
       </div>
       <div className="text-center bg-white right-0 w-[50%] p-10 m-14">
-        <img className="w-72 h-80 mx-auto rounded-full" src={loginUser?.image} alt="user" />
+        <img
+          className="w-72 h-80 mx-auto rounded-full"
+          src={loginUser?.image}
+          alt="user"
+        />
         <p className="text-2xl">{loginUser?.name}</p>
-        <p>{loginUser?.age}</p>
-        <p>{loginUser?.department}</p>
-        <p>{loginUser?.email}</p>
-        <button className="bg-[#728395]" onClick={toggleStatus}>
+        <p className="text-xl">{loginUser?.age}</p>
+        <p className="text-xl">{loginUser?.department}</p>
+        <p className="text-xl flex items-center justify-center"><MdOutlineMail />{loginUser?.email}</p>
+        <button
+          className="bg-[#728395 m-10 text-xl"
+          style={statusStyle} // 이 스타일을 버튼에 적용
+          onClick={toggleStatus}
+        >
           {isWorking ? "부재중" : "근무중"}
         </button>
       </div>
