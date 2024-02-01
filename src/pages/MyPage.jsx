592aeea21efe92facca52a955c5d8a5fb74f66bc
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-02-01 11:17:53 +0900

    mypage style

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,20 +1,24 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { EmployeeContext } from "../context/EmployeeContext";
 
 export default function MyPage() {
   const { loginUser } = useContext(EmployeeContext);
+  const [isWorking, setIsWorking] = useState(loginUser?.isWorking || false);
 
+  const toggleStatus = () => {
+    setIsWorking(!isWorking);
+  };
 
-  
   return (
-  
     <section className="flex w-[100%] bg-[#728395]">
       <div className="p-8 w-[60%] bg-[#728395] bg-blend-multiply"
-      style={{backgroundImage: `url(${loginUser?.image})`, // 이미지 파일의 경로
-      backgroundSize: "cover", // 이미지를 컨테이너에 맞게 조절
-      backgroundPosition: "center",
-      backgroundAttachment: "local",
-      backgroundRepeat: "no-repeat"}}>
+        style={{
+          backgroundImage: `url(${loginUser?.image})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundAttachment: "local",
+          backgroundRepeat: "no-repeat"
+        }}>
         <p>MyPage</p>
       </div>
       <div className="text-center bg-white right-0 w-[50%] p-10 m-14">
@@ -23,7 +27,9 @@ export default function MyPage() {
         <p>{loginUser?.age}</p>
         <p>{loginUser?.department}</p>
         <p>{loginUser?.email}</p>
-        <p>{loginUser?.isworking ? "부재중" : "근무중"}</p>
+        <button className="bg-[#728395]" onClick={toggleStatus}>
+          {isWorking ? "부재중" : "근무중"}
+        </button>
       </div>
     </section>
   );
