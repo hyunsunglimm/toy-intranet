6d7c7d1edc412f3b5974361e939d147e2f7395fe
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-02-02 21:25:51 +0900

    employee page styling

--- src/pages/EmployeePage.jsx
+++ src/pages/EmployeePage.jsx
@@ -1,83 +1,48 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState } from "react";
 import { DataContext } from "../context/DataContext";
 import { MdOutlineMail } from "react-icons/md";
-import { MdOutlineWorkOutline } from "react-icons/md";
-import { FaGem } from "react-icons/fa";
+import { Bs0Circle } from "react-icons/bs";
 import { useParams } from "react-router-dom";
-
-const userinfocss = {
-  text: "text-xl flex items-center justify-center",
-};
 
 export default function EmployeePage() {
   const { employees } = useContext(DataContext);
-  const [isLoading, setIsLoading] = useState(true);
   const params = useParams();
-  const employee = employees.find(employee => employee.id === params.id);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []);
-
+  const employee = employees.find((employee) => employee.id === params.id);
 
   return (
-    <section className="flex w-full bg-[#728395] relative">
-      <div
-        className="p-8 w-[50%] bg-[#728395] bg-blend-multiply bg-right-top"
-        style={{
-          backgroundImage: `url(${employee?.image})`,
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <p>MyPage</p>
-      </div>
-      {isLoading ? (
-        <div className="text-center bg-white absolute right-[20%] top-0 w-[30%] h-[90%] p-10 m-14">
-          <div className="animate-pulse w-72 h-80 mx-auto rounded-full bg-gray-400"></div>
-          <p className="animate-pulse text-3xl w-36 h-8 bg-gray-400 rounded-full mx-auto my-4"></p>
-          <div className={`${userinfocss.text} my-2 animate-pulse`}>
-            <div className="w-20 h-8 bg-gray-400 rounded-full"></div>
-          </div>
-          <div className={`${userinfocss.text} my-2 animate-pulse`}>
-            <div className="w-28 h-8 bg-gray-400 rounded-full"></div>
-          </div>
-          <div className={`${userinfocss.text} my-2 animate-pulse`}>
-            <div className="w-24 h-8 bg-gray-400 rounded-full"></div>
-          </div>
-          <div className={`${userinfocss.text} my-2 animate-pulse`}>
-            <span className="text-xl w-16 h-6 bg-gray-400 rounded-full mx-auto"></span>
-          </div>
+    <section className="flex w-full bg-[#DBE9FE]">
+      <div className="w-1/2 p-12 flex flex-col items-center">
+        <div className="p-8 bg-[#F7F7FB] w-[70%] h-[80%] flex justify-center rounded-lg">
+          <div
+            className="p-3 w-[100%] h-[100%] bg-[#CED3DD] rounded-lg"
+            style={{
+              backgroundImage: `url(${employee?.image})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
         </div>
-      ) : (
-        <div className="text-center bg-white absolute right-[20%] top-0 w-[30%] h-[90%] p-10 m-14">
-          <img
-            className="w-72 mx-auto object-cover"
-            src={employee?.image}
-            alt="user"
-          />
-          <p className="text-3xl">{employee?.name}</p>
-          <p className={`${userinfocss.text} my-2`}>
-            <FaGem />
-            {employee?.age}
+        <div className="h-[20%] bg-[#DBE9FE] text-center w-[80%] flex flex-col justify-center">
+          <p className="text-5xl p-2 text-[#7E7E95]">{employee?.name}</p>
+          <p className="text-2xl p-2 text-[#aaaabb]">{employee?.department}</p>
+        </div>
+      </div>
+
+      <div className="w-1/2 bg-[#DBE9FE]">
+        <div className="py-12 w-[85%] h-2/3 border-b-2 border-[#7E7E95] relative">
+          <p className="text-3xl text-[#7E7E95] mb-5">Work status :</p>
+          <p className="text-2xl text-[#7E7E95] flex items-center gap-[50px]">
+            <Bs0Circle />
+            {employee?.isWorking ? "부재 중" : "근무 중"}
           </p>
-          <p className={`${userinfocss.text} my-2`}>
-            <MdOutlineWorkOutline />
-            {employee?.department}
-          </p>
-          <p className={`${userinfocss.text} my-2`}>
+
+          <p className="text-2xl text-[#aaaabb] absolute right-0 bottom-0 p-4 flex items-center gap-1 ">
             <MdOutlineMail />
             {employee?.email}
           </p>
-          <div className={`${userinfocss.text} my-2`}>
-            <span className="text-xl">
-              {employee.isWorking ? "부재중" : "근무중"}
-            </span>
-          </div>
         </div>
-      )}
+      </div>
     </section>
-  )}
-
+  );
+}
