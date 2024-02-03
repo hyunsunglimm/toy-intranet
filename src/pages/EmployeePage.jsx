f4ddfc136ba9ea40face8d96fbd06f9abce821d7
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-02-03 11:38:15 +0900

    부재 신청 내역 확인 구현

--- src/pages/EmployeePage.jsx
+++ src/pages/EmployeePage.jsx
@@ -1,17 +1,22 @@
 import { useContext, useState } from "react";
 import { DataContext } from "../context/DataContext";
 import { MdOutlineMail } from "react-icons/md";
-import { Bs0Circle } from "react-icons/bs";
 import { useParams } from "react-router-dom";
 
 export default function EmployeePage() {
   const { employees } = useContext(DataContext);
   const params = useParams();
   const employee = employees.find((employee) => employee.id === params.id);
+  console.log(employee?.reasonForAbsence);
+
+  /* 토글 버튼의 클래스 */
+  const toggleButtonClass = employee?.isWorking
+    ? "bg-green-500 text-white"
+    : "bg-red-500 text-white";
 
   return (
     <section className="flex w-full bg-[#DBE9FE]">
-      <div className="w-1/2 p-12 flex flex-col items-center">
+      <div className="w-1/2 p-[80px] flex flex-col items-center">
         <div className="p-8 bg-[#F7F7FB] w-[70%] h-[80%] flex justify-center rounded-lg">
           <div
             className="p-3 w-[100%] h-[100%] bg-[#CED3DD] rounded-lg"
@@ -30,12 +35,18 @@ export default function EmployeePage() {
       </div>
 
       <div className="w-1/2 bg-[#DBE9FE]">
-        <div className="py-12 w-[85%] h-2/3 border-b-2 border-[#7E7E95] relative">
-          <p className="text-3xl text-[#7E7E95] mb-5">Work status :</p>
-          <p className="text-2xl text-[#7E7E95] flex items-center gap-[50px]">
-            <Bs0Circle />
-            {employee?.isWorking ? "부재 중" : "근무 중"}
-          </p>
+        <div className="py-[80px] w-[85%] h-2/3 border-b-2 border-[#7E7E95] relative">
+          <p className="text-4xl text-[#7E7E95] mb-5">Work status :</p>
+          <div className="flex items-center">
+            <button className={`p-2 rounded ${toggleButtonClass}`}>
+              {employee?.isWorking ? "근무 중" : "부재 중"}
+            </button>
+            {!employee?.isWorking && (
+              <p className="text-2xl text-[#7E7E95]">
+                ({employee?.reasonForAbsence || "부재 사유를 입력해주세요!"})
+              </p>
+            )}
+          </div>
 
           <p className="text-2xl text-[#aaaabb] absolute right-0 bottom-0 p-4 flex items-center gap-1 ">
             <MdOutlineMail />
