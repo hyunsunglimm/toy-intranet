f2d44be6632627b3ad89748973eab570a7e94928
Now-h <150241213+Now-h@users.noreply.github.com>
2024-02-05 20:16:47 +0900

    공지갤러리 슬라이더 구현

--- src/components/NoticeGallery.jsx
+++ src/components/NoticeGallery.jsx
@@ -1,20 +1,37 @@
-import { useContext } from "react";
+import { useContext} from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import NoticeCard from "./NoticeCard";
 import { DataContext } from "../context/DataContext";
 
 export default function NoticeGallery() {
   const { notices } = useContext(DataContext);
+  
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  }
 
   return (
     <div className="bg-blue-100 p-4 rounded-md">
       <p className="mb-4 text-center uppercase text-blue-400 font-bold text-[20px]">
         notice gallery
       </p>
-      <ul className="grid grid-cols-4 gap-4">
+      {/* slick 라이브러리 로직 */}
+      <div className="slider-container">
+      <Slider {...settings}>
         {notices.map((notice) => (
-          <NoticeCard key={notice.id} notice={notice} />
+          <div key={notice.id}>
+          <NoticeCard notice={notice} />
+          </div>
         ))}
-      </ul>
+        </Slider>
+      </div>
     </div>
   );
 }
