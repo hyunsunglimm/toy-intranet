a25c19b9f1b36a4685ecb9a253c8a936e782220a
hyunsunglimm <gustjd3834@naver.com>
2024-02-07 15:47:32 +0900

    메인페이지 반응형 레이아웃

--- src/data/skeleton.js
+++ src/data/skeleton.js
@@ -1,3 +1,16 @@
-export const NOTICE_SKELETON_ARRAY = [1, 2, 3, 4];
+function getNoticeSkeletonArray() {
+  if (innerWidth > 1280) {
+    return [1, 2, 3, 4];
+  } else if (innerWidth <= 1280 && innerWidth > 1024) {
+    return [1, 2, 3];
+  } else if (innerWidth <= 1024 && innerWidth > 768) {
+    return [1, 2];
+  } else {
+    return [1];
+  }
+}
 
-export const EMPLOYEE_SKELETON_ARRAY = [1, 2, 3, 4, 5, 6, 7, 8];
+export const NOTICE_SKELETON_ARRAY = getNoticeSkeletonArray();
+
+export const EMPLOYEE_SKELETON_ARRAY =
+  window.innerWidth > 1024 ? [1, 2, 3, 4, 5, 6, 7, 8] : [1, 2, 3, 4, 5, 6];
