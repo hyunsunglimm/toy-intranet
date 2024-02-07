9ae6e4afc3018f89ee4f2bcd3b344dc6ca2129fd
김민재 <kimminjae@gimminjaeui-MacBookPro.local>
2024-02-07 15:16:04 +0900

    상세페이지 스켈레톤 UI 스타일수정

--- src/components/skeleton/SkeletonEmployeePage.jsx
+++ src/components/skeleton/SkeletonEmployeePage.jsx
@@ -14,17 +14,17 @@ export default function SkeletonEmployeePage() {
         <div className="absolute top-[-70px] left-1/2 transform -translate-x-1/2 bg-slate-300 h-[140px] w-[140px] rounded-full"></div>
         <div className="mt-[60px]">
           <div className="text-center mb-3">
-            <div className="h-6 bg-slate-400 rounded w-[130px] mx-auto"></div>
-            <div className="h-4 bg-slate-400 rounded w-[100px] mx-auto mt-2 mb-[30px]"></div>
+            <div className="h-6 bg-slate-400 rounded-md w-[130px] mx-auto"></div>
+            <div className="h-4 bg-slate-400 rounded-md w-[100px] mx-auto mt-2 mb-[30px]"></div>
           </div>
           <div className="bg-white/10 rounded-md border-[1px] border-slate-400/30 p-4">
-            <div className="h-5 bg-slate-400 rounded w-1/2 mx-auto mb-4"></div>
-            <div className="h-5 bg-slate-400 rounded w-4/5 mx-auto "></div>
-            <div className="h-5 bg-slate-400 rounded w-4/5 mx-auto m-1"></div>
-            <div className="h-5 bg-slate-400 rounded w-4/5 mx-auto m-1"></div>
+            <div className="h-5 bg-slate-400 rounded-md w-1/2 mx-auto mb-4"></div>
+            <div className="h-5 bg-slate-400 rounded-md w-4/5 mx-auto "></div>
+            <div className="h-5 bg-slate-400 rounded-md w-4/5 mx-auto m-1"></div>
+            <div className="h-5 bg-slate-400 rounded-md w-4/5 mx-auto m-1"></div>
           </div>
           <div className="mt-10">
-            <div className="h-4 bg-slate-400 rounded w-3/4 mx-auto mb-[55px]"></div>
+            <div className="h-5 bg-slate-400 rounded-md w-3/4 mx-auto mb-[55px]"></div>
           </div>
         </div>
       </div>
