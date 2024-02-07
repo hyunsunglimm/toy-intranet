ec08d2ec8ab81f21253bf4883868278050ab8e94
hyunsunglimm <gustjd3834@naver.com>
2024-02-07 13:33:30 +0900

    직원 상세페이지 이미지 수정 기능 추가

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -4,13 +4,13 @@ export default function ImageUpload({ message, handleChange, file }) {
       <p className="mb-2 text-slate-300">Image</p>
       <input
         className="hidden"
-        id="input-userImg"
+        id="userImage-upload"
         type="file"
         accept="image/*"
         onChange={handleChange}
       />
       <label
-        htmlFor="input-userImg"
+        htmlFor="userImage-upload"
         className="block w-[100px] h-[100px] border-[1px] bg-white/0 border-slate-400/30 hover:border-slate-400/50 rounded-full cursor-pointer mx-auto hover:bg-white/10 transition"
       >
         {file && (
